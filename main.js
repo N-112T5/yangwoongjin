cc411d5b0a55ed895494baaf15ea2b6acf5e8f0a
N-112T5 <62878804+N-112T5@users.noreply.github.com>
2021-06-01 10:56:40 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+window.onload = function() {
+    const toggleButton = document.querySelector('.toggleButton');
+    const menu = document.querySelector('.navBar_menu');
+
+    toggleButton.addEventListener('click', function(){
+        menu.classList.toggle('active');
+    });
+
+    const linkIntro = document.querySelector('.navBar_link_intro');
+    const linkAbout = document.querySelector('.navBar_link_about');
+    const linkOther = document.querySelector('.navBar_link_other');
+
+    var intro = document.getElementById("intro");
+    var about = document.getElementById("about");
+    var about2 = document.getElementById("about2");
+    var other = document.getElementById("other");
+    home = document.querySelector('.home');
+
+    linkIntro.classList.add('on');
+
+    intro.style.display = "flex";
+    about.style.display = "none";
+    about2.style.display = "none";
+    other.style.display = "none";
+
+    home.addEventListener('click', function() {
+        linkIntro.classList.add('on');
+        linkAbout.classList.remove('on');
+        linkOther.classList.remove('on');
+        
+        intro.style.display = "flex";
+        about.style.display = "none";
+        about2.style.display = "none";
+        other.style.display = "none";  
+    })
+
+    linkIntro.addEventListener('click', function() {
+        linkIntro.classList.add('on');
+        linkAbout.classList.remove('on');
+        linkOther.classList.remove('on');
+        
+        intro.style.display = "flex";
+        about.style.display = "none";
+        about2.style.display = "none";
+        other.style.display = "none";
+    });
+
+    linkAbout.addEventListener('click', function() {
+        linkIntro.classList.remove('on');
+        linkAbout.classList.add('on');
+        linkOther.classList.remove('on');
+
+        intro.style.display = "none";
+        about.style.display = "flex";
+        about2.style.display = "flex";
+        other.style.display = "none";
+    });
+
+    linkOther.addEventListener('click', function() {
+        linkIntro.classList.remove('on');
+        linkAbout.classList.remove('on');
+        linkOther.classList.add('on');
+
+        intro.style.display = "none";
+        about.style.display = "none";
+        about2.style.display = "none";
+        other.style.display = "flex";
+    });
+}
+
